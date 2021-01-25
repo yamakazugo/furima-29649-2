c98a5f4052a07814ff65918bcd132dcd1d75bcf5
yamakazugo <yamakazugo@me.com>
2021-01-25 19:58:24 +0900

    コードの修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,7 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!, only:[:index]
   before_action :set_item, only: [:index, :create]
-  before_action :move_to_index, only: [:index]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @purchase_address = PurchaseAddress.new 
